1b8d01b2df1a2b9a5a3b314f86122dd1fc92dc1c
KetillG <ketillgudmunds+github@gmail.com>
2016-12-20 15:36:11 +0000

    added collabsed color

--- colors.js
+++ colors.js
@@ -6,17 +6,19 @@ document.getElementsByTagName('head')[0].appendChild(script);
 chrome.storage.sync.get({
   defaultColor: "D1D1D1",
   hoverColor: "B1B1B1",
+  collapsedColor: "A1A1A1",
   mButton: true,
   scroll: true
 }, function(items) {
   var bColor = '#' + items.defaultColor;
   var hColor = '#' + items.hoverColor;
+  var cColor = "background-color: #" + items.collapsedColor + "!important;"
   var cButton = "";
   if(items.mButton)
   {
     cButton = "color: transparent !important;"
   }
-  var css = ".comment .expand { margin-right: 3px; padding: 1px; height: 100%; background-image: none !important; position: absolute; top: 0; left: 0; bottom: 0; width: 2em; text-align: center; background-color: " + bColor + ";" + cButton + " font-size: 10px; transition: color .15s,background-color .15s; } .child>.sitetable>.comment>.thing {padding-left: 2.5em !important;}  .commentarea>.sitetable>.comment {padding-left: 2.5em !important;} .commentarea>.sitetable>.comment .thing{padding-left: 2.5em !important; background: #fff; box-shadow: 0px 1px 5px rgba(0,0,0,.16); border: none!important; margin-bottom: 10px; margin-left: 0px !important;}.comment, body.res-commentBoxes .comment, html body.res .comment { position: relative; padding: 10px 10px 10px 2.5em !important; padding-left: 2.5em!important; border: 1px solid #EEE; }.comment .expand:hover { text-decoration: none; background-image: none !important; background-color: " + hColor +";" + cButton + " } .comment>.entry>.tagline>.expand {" + cButton + "} .sitetable>.comment>.child>.sitetable>.comment .thing{ padding-left: 2.5em !important; } .collapsed a.expand {background-color: " + bColor + ";}";
+  var css = ".comment .expand { margin-right: 3px; padding: 1px; height: 100%; background-image: none !important; position: absolute; top: 0; left: 0; bottom: 0; width: 2em; text-align: center; background-color: " + bColor + ";" + cButton + " font-size: 10px; transition: color .15s,background-color .15s; } .child>.sitetable>.comment>.thing {padding-left: 2.5em !important;}  .commentarea>.sitetable>.comment {padding-left: 2.5em !important;} .commentarea>.sitetable>.comment .thing{padding-left: 2.5em !important; background: #fff; box-shadow: 0px 1px 5px rgba(0,0,0,.16); border: none!important; margin-bottom: 10px; margin-left: 0px !important;}.comment, body.res-commentBoxes .comment, html body.res .comment { position: relative; padding: 10px 10px 10px 2.5em !important; padding-left: 2.5em!important; border: 1px solid #EEE; }.comment .expand:hover { text-decoration: none; background-image: none !important; background-color: " + hColor +";" + cButton + " } .comment>.entry>.tagline>.expand {" + cButton + "} .sitetable>.comment>.child>.sitetable>.comment .thing{ padding-left: 2.5em !important; } .collapsed a.expand {background-color: " + bColor + ";} .collapsed>.entry>.tagline>.expand {" + cColor + "}";
   style = document.createElement('style');
   if (style.styleSheet) {
     style.styleSheet.cssText = css;
@@ -29,7 +31,7 @@ chrome.storage.sync.get({
     for (var i = 0; i < queryExpand.length; i++) {
       queryExpand[i].addEventListener("click", function(event) {
         if (event.target.offsetParent.offsetTop < $(window).scrollTop()) {
-          $('html, body').animate({ scrollTop: event.target.offsetParent.offsetTop }, 'fast');
+          $('html, body').animate({ scrollTop: event.target.offsetParent.offsetTop - 100 }, 'fast');
         }
       });
     }
--- options.js
+++ options.js
@@ -2,11 +2,13 @@
 function save_options() {
   var colorD = document.getElementById('defC').value;
   var colorH = document.getElementById('hovC').value;
+  var colorC = document.getElementById('colC').value;
   var mHide = document.getElementById('hides').checked;
   var scroll = document.getElementById('scrolls').checked;
   chrome.storage.sync.set({
     defaultColor: colorD,
     hoverColor: colorH,
+    collapsedColor: colorC,
     mButton: mHide,
     scroll: scroll
   }, function() {
@@ -26,14 +28,18 @@ function restore_options() {
   chrome.storage.sync.get({
     defaultColor: "D1D1D1",
     hoverColor: "B1B1B1",
+    collapsedColor: "A1A1A1",
     mButton: true,
     scroll: true
   }, function(items) {
     document.getElementById('defC').style.backgroundColor = '#' + items.defaultColor;
     document.getElementById('hovC').style.backgroundColor = '#' + items.hoverColor;
+    document.getElementById('colC').style.backgroundColor = '#' + items.collapsedColor;
     document.getElementById('hides').checked = items.mButton;
+    document.getElementById('scrolls').checked = items.mButton;
     document.getElementById('defC').value = "";
     document.getElementById('hovC').value = "";
+    document.getElementById('colC').value = "";
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
